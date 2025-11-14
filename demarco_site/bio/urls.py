88d1e0ca73547b959e90dd844a3cbfa49c5c76af
ruemuri89 <ruemurillo@gmail.com>
2025-11-14 09:23:23 -0600

    Updated urls in demarco_site and bio site.

---
+++ demarco_site/bio/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+app_name = "bio"
+
+urlpatterns = [
+    path("", views.HomeView.as_view(), name="home"),
+    path("biography/", views.BiographyView.as_view(), name="biography"),
+    path("works/", views.WorkListView.as_view(), name="works"),
+    path("works/<slug:slug>/", views.WorkDetailView.as_view(), name="work_detail"),
+    path("ideas-lab/", views.IdeasLabView.as_view(), name="ideas_lab"),
+    path("timeline/", views.TimelineView.as_view(), name="timeline"),
+    path("legacy/", views.LegacyView.as_view(), name="legacy"),
+    path("quotes/", views.QuoteListView.as_view(), name="quotes"),
+]
