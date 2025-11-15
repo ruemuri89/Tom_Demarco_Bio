5ea9ecdbe8269fd61e9da3d26bbd324104514395
Rueben Murillo <ruemurillo@gmail.com>
2025-11-15 14:44:11 -0600

    Inserting test view for debugging

--- demarco_site/demarco_site/urls.py
+++ demarco_site/demarco_site/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from bio.views import test_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("bio.urls"),)
+    path('test/', test_view),  # Add this line
+    path("", include("bio.urls")),
 ]
