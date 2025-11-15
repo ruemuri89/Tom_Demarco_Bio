030e350ebb613938344dd15eaf6ea133a523e9c1
Rueben Murillo <ruemurillo@gmail.com>
2025-11-15 13:50:14 -0600

    Added render to allowed hosts

--- demarco_site/demarco_site/settings.py
+++ demarco_site/demarco_site/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-o$^#$lwzt#xlz_5z&(-^%k2pel1q!nq--)a-scl@cjz65vuayq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://tom-demarco-bio.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
@@ -117,7 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT =   BASE_DIR /'staticfiles'
+STATIC_ROOT =   os.path.join(BASE_DIR /'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
