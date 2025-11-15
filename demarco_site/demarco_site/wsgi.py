6693187754419e2105d2f71da6dfd72bc4ed6852
Rueben Murillo <ruemurillo@gmail.com>
2025-11-15 15:10:16 -0600

    Added temporary debug logic to wsgi.py

--- demarco_site/demarco_site/wsgi.py
+++ demarco_site/demarco_site/wsgi.py
@@ -13,4 +13,10 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'demarco_site.settings')
 
+print("=" * 50)
+print("WSGI APPLICATION STARTING")
+print(f"DJANGO_SETTINGS_MODULE: {os.environ.get('DJANGO_SETTINGS_MODULE')}")
+print(f"DEBUG: {os.environ.get('DEBUG', 'Not Set')}")
+print("=" * 50)
+
 application = get_wsgi_application()
